c9b73a2a93f2e2c5186c07ae02fd0b3abf974c98
Jairo <jairobernaldev@gmail.com>
2019-02-26 14:07:42 +0100

    Ejercicio 1 sIN Extra

--- js/ajax.js
+++ js/ajax.js
@@ -1,11 +1,63 @@
 $(document).ready(function(){
-    var boton = $("#bto buttom");
+    var boton = $("#bto button");
 
     boton.eq(1).click(function(){
         var inputs = $("#campos input");
         var selects = $("#campos input");
+        var datos = false;
+        var valor = [];
+        var key = [];
 
-        
+        inputs.each((i)=>{
+            if(inputs.eq(i).val() != ""){
+                valor.push(inputs.eq(i).val());
+                key.push(selects.eq(i).val());
+                datos = true;
+            }
+        });
+
+        if(datos){
+            console.log("------");
+        }else{
+            $.getJSON("php/api.php?readall", function(res){
+                var dato = res;
+                var tabla =`
+                <hr>
+                <table>
+                <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Email</th>
+                <th>Nota</th>
+                </tr>
+                </thead>
+                <tbody>
+                `;
+
+                dato.forEach(e => {
+                    tabla +=`
+                    <tr>
+                    <td>${e.idalumno}</td>
+                    <td>${e.nombre}</td>
+                    <td>${e.apellido}</td>
+                    <td>${e.edad}</td>
+                    <td>${e.email}</td>
+                    <td>${e.nota}</td>
+                    </tr>
+                `;
+                });
+
+                tabla+=`
+                    </tbody>
+                    </table>
+                `;
+
+                $("body").append("<div>").append(tabla);
+            });
+        }
 
     });
 });
--- php/api.php
+++ php/api.php
@@ -1,6 +1,6 @@
 <?php
     $conexion = mysqli_connect("localhost","root","","alumnos");
-    
+    mysqli_query($conexion,"SET NAMES 'utf8'");
     if(isset($_GET["tablas"])){
         $tablas = mysqli_query($conexion,"SHOW COLUMNS FROM alumno");
         while($arrayTabla = mysqli_fetch_assoc($tablas)){
@@ -10,8 +10,13 @@
 
     if(isset($_GET["readall"])){
         $datos = mysqli_query($conexion, "SELECT * FROM alumno");
-        while($arrayDatos = mysqli_fetch_assoc($datos)){
-            echo json_encode($arrayDatos);
+        
+        $a=array();
+        $i = 0;
+        while($arrayDatos = mysqli_fetch_array($datos, MYSQLI_ASSOC)){
+            $a[$i] = $arrayDatos;
+            $i++;
         }
+        echo json_encode($a);
     }
 ?>
